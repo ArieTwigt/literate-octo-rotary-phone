2c1360a9926d1477795d570236643885b075276e
Arie Twigt <twigt.arie@gmail.com>
2023-04-24 14:53:48 +0200

    added examples for day 1

---
+++ my_script.py
@@ -0,0 +1,8 @@
+#%% get the input of the name
+name = input("Wat is je naam:\n")
+print(f"Hello {name}!")
+
+#%% get the input of the age
+age = input("What is your age?")
+print(f"You are {age} old. ")
+# %%
